ed28199545fdadf64ffdcf23a44e92e150b50c85
calvinator42000 <wolfmanfranz@gmail.com>
2025-10-02 13:47:57 -0400

    Switch context to rbsp context when parsing rbsp

--- src/nal.c
+++ src/nal.c
@@ -1,22 +1,39 @@
 #include <nal.h>
 
+extern Context* ctx;
+
 NAL_Unit* nal_unit(size_t NumBytesInNalUnit) {
-    NAL_Unit* nal_unit = malloc(sizeof(NAL_Unit));
-    nal_unit->nuh = nal_unit_header();
-    nal_unit->NumBytesInRbsp = 0;
-    nal_unit->rbsp_byte = malloc(sizeof(uint8_t) * NumBytesInNalUnit - 2);
+    NAL_Unit* nal = malloc(sizeof(NAL_Unit));
+    nal->nuh = nal_unit_header();
+    nal->NumBytesInRbsp = 0;
+    nal->rbsp_byte = malloc(sizeof(uint8_t) * NumBytesInNalUnit - 2);
     for (size_t i = 2; i < NumBytesInNalUnit; i++) {
         if (i + 2 < NumBytesInNalUnit && next_bits(24) == 0x000003) {
-            nal_unit->rbsp_byte[nal_unit->NumBytesInRbsp++] = b();
-            nal_unit->rbsp_byte[nal_unit->NumBytesInRbsp++] = b();
+            nal->rbsp_byte[nal->NumBytesInRbsp++] = b();
+            nal->rbsp_byte[nal->NumBytesInRbsp++] = b();
             i += 2;
             f(8, emulation_prevention_three_byte);
         } else {
-            nal_unit->rbsp_byte[nal_unit->NumBytesInRbsp++] = b();
+            nal->rbsp_byte[nal->NumBytesInRbsp++] = b();
         }
     }
-    nal_unit->rbsp_byte = realloc(nal_unit->rbsp_byte, sizeof(uint8_t) * nal_unit->NumBytesInRbsp);
-    return nal_unit;
+    nal->rbsp_byte = realloc(nal->rbsp_byte, sizeof(uint8_t) * nal->NumBytesInRbsp);
+
+    // Save current context and switch to RBSP context
+    Context* saved_ctx = ctx;
+    Context* nal_ctx;
+    nal_ctx = malloc(sizeof(Context));
+    nal_ctx->data = nal->rbsp_byte;
+    nal_ctx->size = nal->NumBytesInRbsp;
+    nal_ctx->bit_offset = 0;
+    ctx = nal_ctx;
+
+    // Restore original context
+    ctx->data = NULL;
+    free(ctx);
+    ctx = saved_ctx;
+
+    return nal;
 }
 
 void freeNALUnit(NAL_Unit* nal_unit) {

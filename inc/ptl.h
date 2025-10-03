1d7a24c3251393607e4cf9f686f9eead08c09906
calvinator42000 <wolfmanfranz@gmail.com>
2025-10-03 14:22:00 -0400

    Implement base definitions for ptl values

--- inc/ptl.h
+++ inc/ptl.h
@@ -5,7 +5,17 @@
 
 // 7.3.3.1 General profile, tier, and level status - Rec. ITU-T H.266 (V3) (09/2023)
 typedef struct {
-
+    uint_t general_profile_idc;
+    uint_t general_tier_flag;
+    uint_t general_level_idc;
+    uint_t ptl_frame_only_constraint_flag;
+    uint_t ptl_multilayer_enabled_flag;
+    // general_constraints_info
+    uint_t* ptl_sublayer_level_present_flag;
+    uint_t ptl_reserved_zero_bit;
+    uint_t* sublayer_level_idc;
+    uint_t ptl_num_sub_profiles;
+    uint_t* general_sub_profile_idc;
 } Profile_Tier_Level;
 
 Profile_Tier_Level* profile_tier_level(uint_t profileTierPresentFlag, uint_t MaxNumSubLayerMinus1);

92cf0edb978dfd349e6d45aac898dc69fce3c009
calvinator42000 <wolfmanfranz@gmail.com>
2025-10-03 14:26:12 -0400

    Implement definitions for ptl variables before general_constraints_info

--- src/ptl.c
+++ src/ptl.c
@@ -3,6 +3,14 @@
 Profile_Tier_Level* profile_tier_level(uint_t profileTierPresentFlag, uint_t MaxNumSubLayerMinus1) {
     Profile_Tier_Level* ptl = malloc(sizeof(Profile_Tier_Level));
 
+    if (profileTierPresentFlag) {
+        ptl->general_profile_idc = u(7);
+        ptl->general_tier_flag = u(1);
+    }
+    ptl->general_level_idc = u(8);
+    ptl->ptl_frame_only_constraint_flag = u(1);
+    ptl->ptl_multilayer_enabled_flag = u(1);
+
     return ptl;
 }
 

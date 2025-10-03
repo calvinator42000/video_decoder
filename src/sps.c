b1152854745075ab94ff197ccf1f5b4b0002fa5d
calvinator42000 <wolfmanfranz@gmail.com>
2025-10-03 13:46:22 -0400

    Define all sps values before profile_tier_level

--- src/sps.c
+++ src/sps.c
@@ -2,6 +2,17 @@
 
 Sequence_Parameter_Set* seq_parameter_set_rbsp() {
     Sequence_Parameter_Set* sps = malloc(sizeof(Sequence_Parameter_Set));
+
+    sps->sps_seq_parameter_set_id = u(4);
+    sps->sps_video_parameter_set_id = u(4);
+    sps->sps_max_sublayers_minus1 = u(3);
+    sps->sps_chroma_format_idc = u(2);
+    sps->sps_log2_ctu_size_minus5 = u(2);
+    sps->sps_ptl_dpb_hrd_params_present_flag = u(1);
+    if (sps->sps_ptl_dpb_hrd_params_present_flag) {
+        // profile_tier_level
+    }
+
     return sps;
 }
 
